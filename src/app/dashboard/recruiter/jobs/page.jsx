445be9318a3c55b07d5a96fd7b286c632c389006
Syed-Shabok <syed.shabok@gmail.com>
2026-06-09 01:21:04 +0600

    Added Create Job Form.

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecruiterJobsPage = () => {
+  return (
+    <div>
+      <h2>Recruiter Jobs Page</h2>
+    </div>
+  );
+};
+
+export default RecruiterJobsPage;
